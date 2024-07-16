d3c1afc8b3da65e80648a247f5c52bbc7e498179
nftmoondogs <nftmoondogs@gmail.com>
2024-07-16 05:59:55 +0530

    newcommit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -152,7 +152,7 @@ export default function Index() {
             </span>{" "}
             Fair Launch on {" "}
             <Link
-              to="https://pump.fun/7nfvgfXhfE5yPt5ESgr6hNv6wc15eV6y2x7kAbRtpump"
+              to="https://pump.fun/board"
               className="font-extrabold bg-gradient-to-r from-blue-800 via-blue-300 to-blue-50 text-transparent bg-clip-text bg-300% animate-gradient hover:text-emerald-500"
             >
               Pump.fun
